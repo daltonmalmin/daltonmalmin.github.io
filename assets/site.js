86fd8b10adf3eaa64db4ca545d9bffefe7e21ab6
Dalton Malmin <dalton.w.malmin@vanderbilt.edu>
2026-07-28 19:44:28 -0400

    Delete assets directory

--- assets/site.js
+++
@@ -1,6 +0,0 @@
-const yearTargets = document.querySelectorAll("[data-current-year]");
-const currentYear = new Date().getFullYear();
-
-for (const target of yearTargets) {
-  target.textContent = String(currentYear);
-}
